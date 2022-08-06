5520a0cccc132e184dbde224d24231a286734784
RomainC75 <rom.chenard@gmail.com>
2022-08-06 15:16:01 +0200

    AddProductToBasket + @mui

--- src/components/AddProductToBasket.tsx
+++ src/components/AddProductToBasket.tsx
@@ -1,44 +1,54 @@
-import React,{useState, useContext} from 'react'
-import {ProductInterface} from '../@types/product'
+import React, { useState, useContext } from "react";
+import { ProductInterface } from "../@types/product";
 import { CartContext } from "../context/cart.context";
-import { PopulatedProductToOrderInterface } from '../@types/product';
+import { PopulatedProductToOrderInterface } from "../@types/product";
 import { cartContextInterface } from "../@types/cartContext.type";
 
-interface ProductProps {
-    product: ProductInterface;
-  }
+import Button from "@mui/material/Button";
 
-const getQuantityAlreadyInBasket = (cartState:PopulatedProductToOrderInterface[],currentProduct:ProductInterface) =>{
-  const productInBasket = cartState.find(populatedProd=>populatedProd.productId._id===currentProduct._id)
-  if(productInBasket){
-    return productInBasket.quantity
-  }
-  return 0
+interface ProductProps {
+  product: ProductInterface;
 }
 
-export const AddProductToBasket = (props: ProductProps):JSX.Element => {
-  const {product}=props
-  const [ quantityState, setQuantityState ] = useState<number>(0)
-    const { addItemToOfflineCart, addItemToOnlineCart, cartState } = useContext(
-        CartContext
-      ) as cartContextInterface;
-
-      let quantityInBasket=getQuantityAlreadyInBasket(cartState, product)
-
-    
-  const handleQuantity = (num:number) =>{
-    console.log(product, quantityInBasket)
-    const maxPossibleQuantityToAdd = product.stockQuantity-quantityInBasket 
-    if(num>=0 && num<=maxPossibleQuantityToAdd ){
-      setQuantityState(num)
-    }
+const getQuantityAlreadyInBasket = (
+  cartState: PopulatedProductToOrderInterface[],
+  currentProduct: ProductInterface
+) => {
+  const productInBasket = cartState.find(
+    (populatedProd) => populatedProd.productId._id === currentProduct._id
+  );
+  if (productInBasket) {
+    return productInBasket.quantity;
   }
+  return 0;
+};
+
+export const AddProductToBasket = (props: ProductProps): JSX.Element => {
+  const { product } = props;
+  const [quantityState, setQuantityState] = useState<number>(0);
+  const { addItemToOfflineCart, addItemToOnlineCart, cartState } = useContext(
+    CartContext
+  ) as cartContextInterface;
+
+  let quantityInBasket = getQuantityAlreadyInBasket(cartState, product);
+
+  const handleQuantity = (num: number) => {
+    console.log(product, quantityInBasket);
+    const maxPossibleQuantityToAdd = product.stockQuantity - quantityInBasket;
+    if (num >= 0 && num <= maxPossibleQuantityToAdd) {
+      setQuantityState(num);
+    }
+  };
 
   return (
     <div className="AddProductToBasktet">
-      <label htmlFor='quantity'>Quantity</label>
-      <input value={quantityState} type="number" onChange={(el)=>handleQuantity(parseInt(el.target.value))}/>
-      
+      <label htmlFor="quantity">Quantity</label>
+      <input
+        value={quantityState}
+        type="number"
+        onChange={(el) => handleQuantity(parseInt(el.target.value))}
+      />
+      <Button variant="contained" onClick={()=>addItemToOnlineCart(product._id,quantityState,product)}>Add</Button>
     </div>
-  )
-}
+  );
+};
