5a6598980a8ea0827e07eadcb8c8b49b69fea36d
RomainC75 <rom.chenard@gmail.com>
2022-08-07 22:18:12 +0200

    ProductDetails suite

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './style/errorMessage.css'
+
+interface errorInterface{
+    message:string;
+}
+
+export const ErrorMessage = (props:errorInterface):JSX.Element => {
+    const {message}=props
+  return (
+    <div className="ErrorMessage">{message}</div>
+  )
+}
--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,41 +1,58 @@
-import axios from 'axios'
-import React,{useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
-import { ProductInterface } from '../@types/product'
-import Carousel from 'react-material-ui-carousel'
-import { Spinner } from './Spinner'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ProductInterface } from "../@types/product";
+import Carousel from "react-material-ui-carousel";
+import { Spinner } from "./Spinner";
+import { ErrorMessage } from "./ErrorMessage";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
-export const ProductDetails = ():JSX.Element => {
-    const [productState, setProductsState] = useState<ProductInterface | null>(null)
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [isLoaded, setIsLoaded] = useState<boolean>(false)
-    const [isError, setIsError ] = useState<boolean>(false)
-    const [errorMessage, setErrorMessage] = useState<string>('')
+export const ProductDetails = (): JSX.Element => {
+  const [productState, setProductsState] = useState<ProductInterface | null>(
+    null
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-    const {productId} = useParams()
+  const { productId } = useParams();
 
-    useEffect(()=>{
-      setIsLoading(true)
-      axios.get(`${API_URL}/product/${productId}`)
-      .then(ans=>{
-        setIsLoaded(true)
-        setIsLoading(false)
-        setProductsState(ans.data)
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`${API_URL}/product/${productId}`)
+      .then((ans) => {
+        setIsLoaded(true);
+        setIsLoading(false);
+        setProductsState(ans.data);
       })
-      .catch(err=>{
-        setIsLoading(false)
-        setIsError(true)
-        setErrorMessage(err.response.data.message)
-      })
-    },[])
+      .catch((err) => {
+        setIsLoading(false);
+        setIsError(true);
+        setErrorMessage(err.response.data.message);
+      });
+  }, []);
 
   return (
     <div className="ProductDetails">
-      <Spinner/>
-      {isLoading && <Spinner/>}
-        <Carousel>{productState?.pictures?.map((item,i)=><img src={item} key={i}/>)}</Carousel>
+      {isLoading && <Spinner />}
+      {isError && <ErrorMessage message={errorMessage} />}
+      {isLoaded && productState && (
+        <div>
+          <Carousel>
+            {productState?.pictures?.map((item, i) => (
+              <img src={item} key={i} />
+            ))}
+          </Carousel>
+          <p>{productState.brand}</p>
+          <h2>{productState.name}</h2>
+          <ul>
+            {/* {productState.caracteristics && productState.caracteristics.map(())} */}
+          </ul>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
