e5b6d9651d2bfc88cb170004800fbf577e5231ce
RomainC75 <rom.chenard@gmail.com>
2022-08-07 21:51:43 +0200

    spinner

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,20 +1,40 @@
 import axios from 'axios'
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { ProductInterface } from '../@types/product'
 import Carousel from 'react-material-ui-carousel'
+import { Spinner } from './Spinner'
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
-export const ProductDetails = () => {
+export const ProductDetails = ():JSX.Element => {
     const [productState, setProductsState] = useState<ProductInterface | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
+    const [isError, setIsError ] = useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = useState<string>('')
 
     const {productId} = useParams()
 
-    axios.get(`${API_URL}/product/${productId}`)
-    .then(ans=>setProductsState(ans.data))
+    useEffect(()=>{
+      setIsLoading(true)
+      axios.get(`${API_URL}/product/${productId}`)
+      .then(ans=>{
+        setIsLoaded(true)
+        setIsLoading(false)
+        setProductsState(ans.data)
+      })
+      .catch(err=>{
+        setIsLoading(false)
+        setIsError(true)
+        setErrorMessage(err.response.data.message)
+      })
+    },[])
+
   return (
     <div className="ProductDetails">
+      <Spinner/>
+      {isLoading && <Spinner/>}
         <Carousel>{productState?.pictures?.map((item,i)=><img src={item} key={i}/>)}</Carousel>
     </div>
   )
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './style/spinner.css'
+
+export const Spinner = () => {
+  return (
+    <div className="spinner"></div>
+  )
+}
