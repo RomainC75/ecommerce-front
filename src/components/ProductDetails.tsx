44a7b6082012c9d7c9095b46f867fc6054119ebf
RomainC75 <rom.chenard@gmail.com>
2022-08-08 15:39:53 +0200

    ProductDetails basic styling ++

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -6,6 +6,9 @@ import Carousel from "react-material-ui-carousel";
 import { Spinner } from "./Spinner";
 import { ErrorMessage } from "./ErrorMessage";
 
+import "./style/productDetails.css";
+import { AddProductToBasket } from "./AddProductToBasket";
+
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
 export const ProductDetails = (): JSX.Element => {
@@ -40,39 +43,47 @@ export const ProductDetails = (): JSX.Element => {
       {isLoading && <Spinner />}
       {isError && <ErrorMessage message={errorMessage} />}
       {isLoaded && productState && (
-        <div>
-          <Carousel>
+        <div className="ProductDetails__container">
+          <Carousel className="ProductDetails__container__carroussel">
             {productState?.pictures?.map((item, i) => (
               <img src={item} key={i} />
             ))}
           </Carousel>
-          <p>{productState.brand}</p>
-          <h2>{productState.name}</h2>
-          <ul>
-            {productState.caracteristics &&
-              Object.keys(productState.caracteristics).map((key: string, i) => {
-                const keyCarac =
-                  productState.caracteristics &&
-                  key in productState.caracteristics &&
-                  productState.caracteristics[key as keyof caracteristicsInterface]
-                    ? productState.caracteristics[key as keyof caracteristicsInterface]
-                    : [];
-                console.log("keyCarac", keyCarac);
-                return (
-                  <li key={`${key}-i`}>
-                    <p>{key}</p>
-                    <ul>
-                      {/* <li>{ productState.caracteristics[key][1]}</li> */}
-                      {Array.isArray(keyCarac) &&
-                        keyCarac.length > 0 &&
-                        keyCarac.map((caracEntry, i) => (
-                          <li key={`${caracEntry}`}>{caracEntry}</li>
-                        ))}
-                    </ul>
-                  </li>
-                );
-              })}
-          </ul>
+          <div className="ProductDetails__container__infos">
+            <p className="brand">{productState.brand.toUpperCase()}</p>
+            <h2>{productState.name}</h2>
+            <ul className="ProductDetails__container__infos__caracteristicsCat">
+              {productState.caracteristics &&
+                Object.keys(productState.caracteristics).map(
+                  (key: string, i) => {
+                    const keyCarac =
+                      productState.caracteristics &&
+                      key in productState.caracteristics &&
+                      productState.caracteristics[
+                        key as keyof caracteristicsInterface
+                      ]
+                        ? productState.caracteristics[
+                            key as keyof caracteristicsInterface
+                          ]
+                        : [];
+                    return (
+                      <li key={`${key}-i`}>
+                        <p>{key}</p>
+                        <ul className="ProductDetails__container__infos__catElement">
+                          {/* <li>{ productState.caracteristics[key][1]}</li> */}
+                          {Array.isArray(keyCarac) &&
+                            keyCarac.length > 0 &&
+                            keyCarac.map((caracEntry, i) => (
+                              <li key={`${caracEntry}`}>{caracEntry}</li>
+                            ))}
+                        </ul>
+                      </li>
+                    );
+                  }
+                )}
+            </ul>
+            <AddProductToBasket product={productState}/>
+          </div>
         </div>
       )}
     </div>
