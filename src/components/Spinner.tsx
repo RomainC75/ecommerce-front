d777c788ad0c470964ada0b3cdb9a32500443470
RomainC75 <rom.chenard@gmail.com>
2022-09-08 21:00:08 +0200

    spinner changed

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 import './style/spinner.css'
 
 export const Spinner = () => {
+  console.log("===>", new Array(10).fill(0))
   return (
-    <div className="spinner"></div>
+    <div className="spinner">
+      {(new Array(12).fill(0).map(x=><div></div>))}
+    </div>
   )
 }
