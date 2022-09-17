6eb662e550d7e978938c6283613866230b50880e
RomainC75 <rom.chenard@gmail.com>
2022-09-17 17:11:16 +0200

    made with love display

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,9 @@ import './style/footer.css'
 
 export const Footer = () => {
   return (
-    <div className="Footer">BioCoop'</div>
+    <div className="Footer">
+      <h3>BioCoop'</h3>
+      <p>Made with ❤️ by Romain Chenard</p>
+    </div>
   )
 }
